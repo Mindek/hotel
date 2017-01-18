2aad15aa4bf9f761a4e0c19df772c0b2e3a214ef
Dominik Chyliński <Sasquatch.dc@gmail.com>
2017-01-18 19:54:03 +0100

    few more components

---
+++ app/contact/contact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'contact',
+    template: `
+    KONTAKT!
+
+`
+})
+export class ContactComponent  {
+
+}
---
+++ app/gallery/galery.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'gallery',
+    template: `
+    Galeria
+
+`
+})
+export class GalleryComponent  {
+
+}
