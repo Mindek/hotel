8ad0a77beda77c6290af327684b2669feb8f2ca5
rafaldziubaczka <dziubaczka.rafal@gmail.com>
2017-01-19 14:35:18 +0100

    contact component

--- app/contact/contact.component.ts
+++ app/contact/contact.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+declare var google: any;
 
 @Component({
     selector: 'contact',
-    template: `
-    KONTAKT!
-
-`
+    moduleId: module.id,
+    templateUrl: 'contact.component.html'
 })
-export class ContactComponent  {
+export class ContactComponent implements OnInit {
+    ngOnInit() {
+        var myLocation = new google.maps.LatLng(50.029418, 19.925624);
+        var mapProp = {
+            center: myLocation,
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
 
+        var marker = new google.maps.Marker({
+            position: myLocation,
+            title: "Hotel Panorama"
+        });
+
+        var map = new google.maps.Map(document.getElementById("map1"), mapProp);
+        marker.setMap(map);
+    }
 }
